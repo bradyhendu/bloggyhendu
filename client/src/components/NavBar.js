0e53ab56aec67fa4d819118fceaf97bc138b90a7
Brady Henderson <bradyhendu@gmail.com>
2023-12-18 18:12:05 -0500

    Finishes simple Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,22 +3,23 @@ import logo from '../images/logo.png'
 import '../styles/NavBar.scss'
 
 const NavBar = () => {
+
+    
+
   return (
     <nav className="navbar navbar-expand-md navbar-light navbar-content">
         <div className='container-fluid'>
             <a className="navbar-brand" href="#">
                 <img src={logo} width="125" height="30" alt="" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <div className="collapse navbar-collapse navbar-right" id="navbarNav">
+                <div className="navbar-nav">
+                    <a className="nav-link" aria-current="page" href="#">Login</a>
+                    <a className="nav-link" href="#">Register</a>
                 </div>
             </div>
         </div>
