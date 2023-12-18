58ef600248298e031b1f470fabcf59ea7a0f8452
Brady Henderson <bradyhendu@gmail.com>
2023-12-18 17:31:06 -0500

    Navbar expandable menu now opens, but doesn't close

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,24 +5,23 @@ import '../styles/NavBar.scss'
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-light navbar-content">
-        <a className="navbar-brand" href="#">
-            <img src={logo} width="125" height="30" alt="" />
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className='container-fluid'>
+            <a className="navbar-brand" href="#">
+                <img src={logo} width="125" height="30" alt="" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div className="collapse navbar-collapse navbar-right" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Register</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </div>
+            </div>
         </div>
-
     </nav>
   )
 }
