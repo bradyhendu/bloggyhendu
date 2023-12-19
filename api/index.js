84b4115f9585d97eb97081a5edbadeec5c4cf705
Brady Henderson <bradyhendu@gmail.com>
2023-12-19 16:51:23 -0500

    Uses bcrypt to hide the password

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 const UserModel = require('./models/User');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; 
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,11 +17,14 @@ mongoose.connect('mongodb+srv://blogAdmin:LlF78kOJ0feZgMyE@blogcluster.48qnofa.m
 app.post('/register', async (req, res) => {
     const {username, password, firstName, lastName, email} = req.body;
     try{
-        const userDoc = await UserModel.create({username, password, firstName, lastName, email});
+        const userDoc = await UserModel.create({username, password:bcrypt.hashSync(password, saltRounds), firstName, lastName, email});
         res.json(userDoc);
     } catch(err) {
-        console.log(err);
-        res.status(400).json(err);
+        if(err.code === 11000){
+            res.status(400).json({message: 'Username or email already exists'});
+        } else {
+            res.status(500).json({message: 'Something went wrong'});
+        }
     }
 });
 
