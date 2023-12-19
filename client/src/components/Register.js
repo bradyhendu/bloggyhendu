3d068ef770da0d1c8fb38b3dfdcca2393e628ac2
Brady Henderson <bradyhendu@gmail.com>
2023-12-19 13:57:05 -0500

    Moves package.json into main folder

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -11,10 +11,10 @@ const Register = () => {
                     <h1 className="text-center my-2">Register</h1>
                     <small className="text-center">Welcome to bloggyhendu! Please create an account below</small>
                     <hr className="w-100" />
-                    <input type="text" placeholder="First Name" className="form-control my-3 " />
-                    <input type="text" placeholder="Last Name" className="form-control my-3 " />
-                    <input type="email" placeholder="Email" className="form-control my-3 " />
-                    <input type="text" placeholder="Username" className="form-control my-3 " />
+                    <input type="text" placeholder="First Name" className="form-control my-3" />
+                    <input type="text" placeholder="Last Name" className="form-control my-3" />
+                    <input type="email" placeholder="Email" className="form-control my-3" />
+                    <input type="text" placeholder="Username" className="form-control my-3" />
                     <input type="password" placeholder="Password" className="form-control my-3" />
                     <button className="btn custom-bg my-3">Register</button>
                     <p className="text-center">Already have an account? <a href="/login">Login</a></p>
