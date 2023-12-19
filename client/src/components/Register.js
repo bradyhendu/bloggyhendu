2480b1fa1208f50cd0488e83f96ffb9024d4cad7
Brady Henderson <bradyhendu@gmail.com>
2023-12-19 15:56:42 -0500

    Sets up error handling but needs fixes

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -6,24 +6,41 @@ const Register = () => {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
 
-  function register(e) {
+  async function register(e) {
     e.preventDefault();
-    fetch('http://localhost:4000/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({username, password, firstName, lastName, email})
-    })
+    try{
+        const response = await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password, firstName, lastName, email})
+        });
+
+        console.log(response);
+
+
+    } catch(err){ //TODO: handle errors
+        if(!err?.response){
+            setError('Something went wrong');
+        } else if (err.response.status === 400){
+            setError('Bad response');
+        } else if (err.response.status === 500){
+            setError('Something went wrong');
+        } else {
+            setError('Something went wrong');
+        }
+    }
   }
 
   return (
     <>
-        <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+        <div className="container-fluid d-flex justify-content-center align-items-center flex-column vh-100">
             <div className=' col-6 d-flex justify-content-center align-items-center register-box rounded-4 '>
                 <form className='d-flex flex-column' onSubmit={register}>
                     <h1 className="text-center my-2">Register</h1>
@@ -38,6 +55,12 @@ const Register = () => {
                     <p className="text-center">Already have an account? <a href="/login">Login</a></p>
                 </form>
             </div>
+            {
+                error && 
+                <div class="alert alert-danger my-3" role="alert">
+                    {error}
+                </div>
+            }
         </div>
     </>
   )
