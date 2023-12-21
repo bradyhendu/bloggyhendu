927031e035006216cc0f430251e1cb6d80dbc31d
Brady Henderson <bradyhendu@gmail.com>
2023-12-21 16:14:27 -0500

    Adds routes and base pages for Create and Profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar";
 import Post from "./components/Post";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Create from "./components/Create";
+import Profile from "./components/Profile";
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Post />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
