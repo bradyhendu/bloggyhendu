908c68bf940698742c173b39f06929da36587088
Brady Henderson <bradyhendu@gmail.com>
2023-12-26 17:48:14 -0500

    Creates the Homepage and changes the post to take info

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import NavBar from "./components/NavBar";
-import Post from "./components/Post";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Create from "./components/Create";
 import Profile from "./components/Profile";
+import HomePage from "./components/HomePage";
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Post />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/create" element={<Create />} />
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({title, description, content, image, createdAt}) => {
   return (
-    <div>
-      Post
+    <div className=''>
+      <div className='card mb-3'>
+        <img src={image} className='card-img-top' alt={title} />
+        <div className='card-body'>
+          <h5 className='card-title'>{title}</h5>
+          <p className='card-text'>{description}</p>
+          <p className='card-text'><small className='text-muted'>Created on {new Date(createdAt).toLocaleDateString()}</small></p>
+        </div>
+      </div>
     </div>
   )
 }
