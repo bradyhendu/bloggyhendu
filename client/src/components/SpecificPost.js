d9bc2bc4e727b8b1e7db64471c79f675d2aaa729
Brady Henderson <bradyhendu@gmail.com>
2023-12-28 15:53:32 -0500

    Adds the single page view for a blog post

--- client/src/components/SpecificPost.js
+++ client/src/components/SpecificPost.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
-import { formatISO9075 } from 'date-fns'
+import { format } from 'date-fns'
 
 
 const SpecificPost = () => {
@@ -17,7 +17,17 @@ const SpecificPost = () => {
 
   return (
     <div>
-      This is my specific post
+        {post &&
+            <div className='my-3 mx-3' style={{ fontFamily: 'Roboto' }}>
+                <h1 className='text-center'>{post.title}</h1>
+                <h5 className='text-center text-muted'>Posted on {format(new Date(post.createdAt), 'MM/dd/yyyy')} by {post.author.firstName + ' ' + post.author.lastName}</h5>
+                <div className='d-flex justify-content-center my-2'>
+                    <img src={'http://localhost:4000/' + post.image} className='img-fluid rounded w-75' alt={post.title} />
+                </div>
+                <h4 className='text-center'>{post.description}</h4>
+                <div style={{fontSize: '18px'}} dangerouslySetInnerHTML={{__html:post.content}}></div>
+            </div>
+        }
     </div>
   )
 }
