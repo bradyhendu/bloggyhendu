db0d5a7bc81b9614e94ef93c6e51b1a8541bd128
Brady Henderson <bradyhendu@gmail.com>
2023-12-28 18:12:47 -0500

    Adds basic footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <nav class="navbar fixed-bottom" style={{backgroundColor: '#EDE1D1'}}>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Fixed bottom</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Footer
