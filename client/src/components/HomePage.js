bb988091a475af0db578b1cd6e1d1e1b03ccaac9
Brady Henderson <bradyhendu@gmail.com>
2023-12-27 19:02:39 -0500

    If there are no posts (or if there is a connection error), there is a message that user's can see

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -18,6 +18,12 @@ const HomePage = () => {
         {posts.length > 0 && posts.map(post => (
             <Post key={post._id} {...post}/>
         ))}
+        {posts.length === 0 && 
+            <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
+                <h2 className='text-center text-muted'>No posts yet, check back later</h2>
+                <h5 className='text-center text-muted'>Or <a href='/create' style={{color: '#6A704C'}}>create one</a> yourself!</h5>
+            </div>
+        }
     </div>
   )
 }
