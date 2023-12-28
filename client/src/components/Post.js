8fdc3e64bb2ffbe3dcb1994dbf124f1c2b757c2f
Brady Henderson <bradyhendu@gmail.com>
2023-12-27 19:02:11 -0500

    The card will now link to the specific post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { formatISO9075 } from 'date-fns'
+import { Link } from 'react-router-dom';
 
-const Post = ({title, description, image, createdAt, author}) => {
+const Post = ({title, description, image, createdAt, author, _id}) => {
   return (
-    <div className='card my-3 mx-2' style={{maxWidth: '500px'}}>
-      <div className='row g-0'>
-          <div>
-          <img src={'http://localhost:4000/' + image} className='img-fluid rounded w-100' style={{height: '350px'}} alt={title} />
-          </div>
-        <div className='col-md-10'>
-          <div className='card-body'>
-            <h5 className='card-title'>{title}</h5>
-            <p className='card-text'>{description}</p>
-            <p className='card-text'><small className='text-muted'>Posted on {formatISO9075(new Date(createdAt))} by {author.firstName + ' ' + author.lastName}</small></p>
+    <Link to={'/post/' + _id} className='text-decoration-none text-dark'>
+      <div className='card my-3 mx-2' style={{maxWidth: '500px'}}>
+        <div className='row g-0'>
+            <div>
+            <img src={'http://localhost:4000/' + image} className='img-fluid rounded w-100' style={{height: '350px'}} alt={title} />
+            </div>
+          <div className='col-md-10'>
+            <div className='card-body'>
+              <h5 className='card-title'>{title}</h5>
+              <p className='card-text'>{description}</p>
+              <p className='card-text'><small className='text-muted'>Posted on {formatISO9075(new Date(createdAt))} by {author.firstName + ' ' + author.lastName}</small></p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
