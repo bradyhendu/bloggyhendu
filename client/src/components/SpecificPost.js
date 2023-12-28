14111586a11c70721283c59f6dbc6e18a61f2b90
Brady Henderson <bradyhendu@gmail.com>
2023-12-27 19:09:05 -0500

    Sets the post after communicating with the backennd

--- client/src/components/SpecificPost.js
+++ client/src/components/SpecificPost.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom';
+import { formatISO9075 } from 'date-fns'
+
 
 const SpecificPost = () => {
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/post/' + id).then(res => 
+            res.json().then(post => 
+                setPost(post)
+            )
+        )
+    }, [id]);
+
   return (
     <div>
       This is my specific post
