6d8d40c816e0534261f6bd4b0f601e320f152301
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 10:50:32 -0500

    Changes card to appear instagram like

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { formatISO9075 } from 'date-fns'
 import { Link } from 'react-router-dom';
+import { formatDistanceToNow } from 'date-fns'
 
 const Post = ({title, description, image, createdAt, author, _id}) => {
   return (
-    <Link to={'/post/' + _id} className='text-decoration-none text-dark'>
-      <div className='card my-3 mx-2' style={{maxWidth: '500px'}}>
+      <div className='card my-3 mx-2' style={{maxWidth: '400px', fontSize: '.75rem'}}>
         <div className='row g-0'>
-            <div>
-            <img src={'http://localhost:4000/' + image} className='img-fluid rounded w-100' style={{height: '350px'}} alt={title} />
+            <div className='col-md-12 d-flex align-items-center m-1'>
+              <img src={'http://localhost:4000/' + author.profilePicture} className='img-fluid rounded-circle me-2' style={{height: '35px', width: '35px'}} alt={author.username} />
+              <p className='text-center mb-0 fw-medium' style={{marginRight: '5px'}}>{author.username}</p>
+              <p className='text-center mb-0'> • {formatDistanceToNow(new Date(createdAt))} ago</p>  
             </div>
-          <div className='col-md-10'>
-            <div className='card-body'>
-              <h5 className='card-title'>{title}</h5>
-              <p className='card-text'>{description}</p>
-              <p className='card-text'><small className='text-muted'>Posted on {formatISO9075(new Date(createdAt))} by {author.firstName + ' ' + author.lastName}</small></p>
+            <Link to={'/post/' + _id} className='text-decoration-none text-dark'>
+              <div>
+                <img src={'http://localhost:4000/' + image} className='img-fluid rounded w-100' style={{height: '350px'}} alt={title} />
+              </div>
+            <div className='col-md-12'>
+              <div className='card-body'>
+                <h5 className='card-title '>{title}</h5>
+                <p className='card-text'>{description}</p>
+              </div>
             </div>
-          </div>
+            </Link>   
         </div>
       </div>
-    </Link>
   )
 }
 
