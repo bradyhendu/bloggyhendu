5305f6fc3402db4d5dd0f96a83db8d571b8f6d1a
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 12:02:26 -0500

    Changes the path for the profile

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/create" element={<Create />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:username" element={<Profile />} />
         <Route path="/post/:id" element={<SpecificPost />} />
         <Route path="/delete/:id" element={<Delete />} />
         <Route path="/edit/:id" element={<Edit />} />
