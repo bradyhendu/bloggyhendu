c668ae7e28a18cc14682b72b7933bb8879a8b74e
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 14:00:44 -0500

    The user's profile picture and username will direct you to their page

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -7,8 +7,10 @@ const Post = ({title, description, image, createdAt, author, _id}) => {
       <div className='card my-3 mx-2' style={{maxWidth: '400px', fontSize: '.75rem'}}>
         <div className='row g-0'>
             <div className='col-md-12 d-flex align-items-center m-1'>
-              <img src={'http://localhost:4000/' + author.profilePicture} className='img-fluid rounded-circle me-2' style={{height: '35px', width: '35px'}} alt={author.username} />
-              <p className='text-center mb-0 fw-medium'>{author.username}</p>
+              <Link to={'/profile/' + author.username} className='text-decoration-none text-dark d-flex align-items-center'>
+                <img src={'http://localhost:4000/' + author.profilePicture} className='img-fluid rounded-circle me-2' style={{height: '35px', width: '35px'}} alt={author.username} />
+                <p className='text-center mb-0 fw-medium'>{author.username}</p>
+              </Link>
               <p className='text-center mb-0 mx-1'>•</p>
               <p className='text-center mb-0'>{formatDistanceToNow(new Date(createdAt))} ago</p>  
             </div>
