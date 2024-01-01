de8c6954887a1e4376dd5408c8e2e0095fc1c0b9
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 15:54:51 -0500

    Renders the user's posts to the profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Cookies from 'js-cookie'
 import {useParams} from 'react-router-dom'
+
+import Post from './Post'
 
 
 
@@ -8,7 +10,8 @@ import '../styles/Profile.scss'
 
 const Profile = () => {
   let { username } = useParams();
-  const [user, setUser] = React.useState(null);
+  const [user, setUser] = useState(null);
+  const [userPosts, setUserPosts] = useState(null);
   
   let token;
   let tokenUsername;
@@ -25,9 +28,12 @@ const Profile = () => {
 
   useEffect(() => {
     fetch('http://localhost:4000/user/' + username).then(res => 
-        res.json().then(user => 
-            setUser(user)
-        )
+      res.json().then(data => {
+        if(data.user){
+          setUser(data.user);
+          setUserPosts(data.user.userPosts);
+        }
+      }).catch(err => console.log(err))
     )
   }, [username]);
 
@@ -40,20 +46,37 @@ const Profile = () => {
     <>
       {
         user ? (
-          <div className="container-fluid d-flex justify-content-center align-items-center flex-column vh-100">
-            <div className=' col-10 d-flex justify-content-center align-items-center profile-box rounded-4 flex-column'>
-              <h1 className='my-3'>{user.username}'s Profile</h1>
-              <hr className="w-100 line" />
-              <div className="d-flex flex-column">
-                <p><span>Name:</span> {user.firstName} {user.lastName}</p>
-                <p><span>Email:</span> {user.email}</p>
+          <div className="container-fluid d-flex justify-content-center align-items-center flex-column my-5 w-75">
+              <div className="d-flex justify-content-center align-items-center flex-row">
+                <img src={'http://localhost:4000/' + user.profilePicture} className='img-fluid rounded-circle mx-3' style={{height: '125px', width: '125px'}} alt={user.username} />
+                <div className='d-flex flex-column'>
+                  <h1 className='my-3'>{user.username}'s Profile</h1>
+                  <div className='d-flex flex-row'>
+                    <small className='mx-2'><span>Name:</span> {user.firstName} {user.lastName}</small>
+                    <small className='mx-2'><span>Email:</span> {user.email}</small>
+                  </div>
+                </div>
               </div>
               {tokenUsername === username &&
                 <div className="d-flex justify-content-center align-items-center flex-row">
+                  <button className="btn btn-secondary my-3 mx-2" onClick={() => window.location.href = '/profile/edit/' + username}>Edit Profile</button>
                   <button className="btn btn-danger my-3 mx-2" onClick={logout}>Logout</button>
                 </div>
               }
-            </div>
+              <hr className="w-100 line" />
+              <div className="d-flex justify-content-center align-items-center flex-column">
+                <h3 className='my-3'>Posts</h3>
+                <div className="d-flex flex-wrap justify-content-center align-items-center flex-row">
+                {userPosts.length > 0 && userPosts.map(post => (
+                  <Post key={post._id} {...post}/>
+                ))}
+                {userPosts.length === 0 && 
+                    <div className='d-flex justify-content-center align-items-center flex-column'>
+                        <h2 className='text-center text-muted'>This user has no posts yet, check back later</h2>
+                    </div>
+                }
+                </div>
+              </div>
           </div>
         ) : (
           <div className="alert alert-danger my-3" role="alert" id='error'>
