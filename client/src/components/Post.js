64edbca7ba50594bfef72d7eca771854e35e7f5a
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 11:18:03 -0500

    Changes the format of the blog posts

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -8,12 +8,13 @@ const Post = ({title, description, image, createdAt, author, _id}) => {
         <div className='row g-0'>
             <div className='col-md-12 d-flex align-items-center m-1'>
               <img src={'http://localhost:4000/' + author.profilePicture} className='img-fluid rounded-circle me-2' style={{height: '35px', width: '35px'}} alt={author.username} />
-              <p className='text-center mb-0 fw-medium' style={{marginRight: '5px'}}>{author.username}</p>
-              <p className='text-center mb-0'> • {formatDistanceToNow(new Date(createdAt))} ago</p>  
+              <p className='text-center mb-0 fw-medium'>{author.username}</p>
+              <p className='text-center mb-0 mx-1'>•</p>
+              <p className='text-center mb-0'>{formatDistanceToNow(new Date(createdAt))} ago</p>  
             </div>
             <Link to={'/post/' + _id} className='text-decoration-none text-dark'>
               <div>
-                <img src={'http://localhost:4000/' + image} className='img-fluid rounded w-100' style={{height: '350px'}} alt={title} />
+                <img src={'http://localhost:4000/' + image} className='img-fluid w-100' style={{height: '350px'}} alt={title} />
               </div>
             <div className='col-md-12'>
               <div className='card-body'>
