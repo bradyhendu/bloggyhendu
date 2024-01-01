71c5b2635dbef443a075db2805048f8414797096
Brady Henderson <bradyhendu@gmail.com>
2023-12-31 21:04:19 -0500

    Finishes the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <nav class="navbar fixed-bottom" style={{backgroundColor: '#EDE1D1'}}>
-        <div class="container-fluid">
-            <p class="text-center">Created by <a href='https://github.com/bradyhendu'>Brady Henderson</a> © 2023</p>
+      <nav className="navbar fixed-bottom" style={{backgroundColor: '#EDE1D1'}}>
+        <div className="container-fluid d-flex align-items-center">
+            <p className="text-center mb-0">Created by <a href='https://github.com/bradyhendu' rel="noopener noreferrer" style={{color: '#6A704C'}} target='_blank'>Brady Henderson</a> © 2023</p>     
+            <button type="button" onClick={() => window.open('https://github.com/bradyhendu/bloggyhendu/issues', '_blank')} className="btn btn-outline-danger btn-sm">Report a Bug</button>
         </div>
       </nav>
     </>
