532563f73db1de9c7b000a123a9c5bca7f1b9d53
Brady Henderson <bradyhendu@gmail.com>
2024-01-01 09:38:07 -0500

    Adds padding to the bottom of the div containing the posts

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -15,18 +15,20 @@ const HomePage = () => {
     }, []);
 
   return (
-    <div className='my-3 d-flex align-items-center flex-column vh-100'>
-        {posts.length > 0 && posts.map(post => (
-            <Post key={post._id} {...post}/>
-        ))}
-        {posts.length === 0 && 
-            <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
-                <h2 className='text-center text-muted'>No posts yet, check back later</h2>
-                <h5 className='text-center text-muted'>Or <a href='/create' style={{color: '#6A704C'}}>create one</a> yourself!</h5>
-            </div>
-        }
+    <>
+        <div className='my-3 d-flex align-items-center flex-column pb-5'>
+            {posts.length > 0 && posts.map(post => (
+                <Post key={post._id} {...post}/>
+            ))}
+            {posts.length === 0 && 
+                <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
+                    <h2 className='text-center text-muted'>No posts yet, check back later</h2>
+                    <h5 className='text-center text-muted'>Or <a href='/create' style={{color: '#6A704C'}}>create one</a> yourself!</h5>
+                </div>
+            }
+        </div>
         <Footer />
-    </div>
+    </>
   )
 }
 
